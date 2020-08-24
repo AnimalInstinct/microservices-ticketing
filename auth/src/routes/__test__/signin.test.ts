6796f261bf8dded0e7462ff444f0313dbe3df3e7
Alexander Makarov <alexander.makaroff@gmail.com>
2020-08-24 14:20:18 +0700

    Unit tests for Auth service added

---
+++ auth/src/routes/__test__/signin.test.ts
@@ -0,0 +1,65 @@
+import request from 'supertest'
+import { app } from '../../app'
+
+it('returns a 200 on successfull signin', async () => {
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: 'password',
+    })
+    .expect(201)
+  return request(app)
+    .post('/api/users/signin')
+    .send({
+      email: 'test@test.com',
+      password: 'password',
+    })
+    .expect(200)
+})
+
+it("fails and returns 400 when an email that doesn't exists is supplied", async () => {
+  return request(app)
+    .post('/api/users/signin')
+    .send({
+      email: 'test@test.com',
+      password: 'password',
+    })
+    .expect(400)
+})
+
+it('fails and returns 400 when an incorrect password is supplied', async () => {
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: 'password',
+    })
+    .expect(201)
+  await request(app)
+    .post('/api/users/signin')
+    .send({
+      email: 'test@test.com',
+      password: '1',
+    })
+    .expect(400)
+})
+
+it('response with a cookie when given valid credentials', async () => {
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: 'password',
+    })
+    .expect(201)
+  const response = await request(app)
+    .post('/api/users/signin')
+    .send({
+      email: 'test@test.com',
+      password: 'password',
+    })
+    .expect(200)
+
+  expect(response.get('Set-Cookie')).toBeDefined()
+})
--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,12 +1,11 @@
 import express, { Request, Response } from 'express'
-import { currentUser, requireAuth } from '../middlewares'
+import { currentUser } from '../middlewares'
 
 const router = express.Router()
 
 router.get(
   '/api/users/currentuser',
   currentUser,
-  requireAuth,
   (req: Request, res: Response) => {
     res.send({ currentUser: req.currentUser || null })
   }
